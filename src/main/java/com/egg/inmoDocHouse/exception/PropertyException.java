e05879e833ac3bf03742ceea15508f37e238493d
Gabi Iezzi <geiezzi90@gmail.com>
2022-11-25 18:02:42 -0300

    Add Service Property

---
+++ src/main/java/com/egg/inmoDocHouse/exception/PropertyException.java
@@ -0,0 +1,9 @@
+package com.egg.inmoDocHouse.exception;
+
+public class PropertyException extends NullPointerException{
+
+    public PropertyException(){
+        super("Esta propiedad se encuentra creada");
+    }
+
+}
