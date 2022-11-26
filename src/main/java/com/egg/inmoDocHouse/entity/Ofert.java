9bbad4c11f9b4d9bdb90fc98e4a28f1ad3a1e73c
Fernando Agustin Perez <aguss.97@outlook.com>
2022-11-26 10:04:55 -0300

    Ofert entity and repo

---
+++ src/main/java/com/egg/inmoDocHouse/entity/Ofert.java
@@ -0,0 +1,35 @@
+package com.egg.inmoDocHouse.entity;
+
+import ch.qos.logback.core.net.server.Client;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ofert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    private Property idPropery;
+
+    @OneToOne
+    private Client idCliente;
+
+    @OneToOne
+    private Ente idEnte;
+
+    private String message;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateMessage;
+}
---
+++ src/main/java/com/egg/inmoDocHouse/repository/OfertRepository.java
@@ -0,0 +1,8 @@
+package com.egg.inmoDocHouse.repository;
+
+import com.egg.inmoDocHouse.entity.Ofert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfertRepository extends JpaRepository<Ofert, Integer> {
+
+}
