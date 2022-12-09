a8be47d9152895b7bffaed49b4967cdf23c953ac
leonardo <leonardogtaquini@gmail.com>
2022-12-09 20:11:46 -0300

    ultimos cambios donde funciona todo con cambio de imagenes

--- front/src/pages/PreRegistration.jsx
+++ front/src/pages/PreRegistration.jsx
@@ -7,11 +7,11 @@ export const PreRegistration = () => {
     <>
     <Navbar/>
       <div className='contenedor-preregistro d-flex align-items-center justify-content-center'>
-        <div className='d-flex  align-items-center justify-content-center'>
-          <button className='btn btn-lg btn-primary border border-light shadow-lg fw-bold mx-5 h1 p-4'>
+        <div className='d-flex  align-items-center justify-content-center flex-column flex-lg-row flex-sm-row'>
+          <button className='btn btn-lg btn-primary border border-light shadow-lg fw-bold mx-5 h1 p-4 my-4'>
            Soy Inmobiliaria
            </button>
-          <button className='btn btn-lg btn-primary border border-light shadow-lg fw-bold mx-5 h1 p-4'>
+          <button className='btn btn-lg btn-primary border border-light shadow-lg fw-bold mx-5 h1 p-4 my-4'>
             Soy Propietario
           </button>
         </div>
